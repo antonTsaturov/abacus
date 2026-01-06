697ab060ce86b4b800062c3d1246eba69f61784e
Anton Tsaturov <antonotna9@gmail.com>
2026-01-06 20:52:39 +0300

    Update vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Optional: allows external access
+    allowedHosts: [
+      'abacus-game.ru',
+      'www.abacus-game.ru', // Include www subdomain if needed
+      'localhost' // Keep localhost for local development
+    ]
+  }
 })
